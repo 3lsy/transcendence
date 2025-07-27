2c66ff4c3eaff87bb9393bde69df22ae1cd3be25
3lsy <echavez-@student.42.fr>
2025-07-27 19:40:20 +0200

    fix vault waiting grafana + ELK stack

--- services/logs-management/elasticsearch/conf/start.sh
+++ services/logs-management/elasticsearch/conf/start.sh
@@ -14,23 +14,48 @@ export VAULT_ADDR=https://vault:8200
 export VAULT_SKIP_VERIFY=true              # Self-signed certificates require this
 export HOME=/tmp
 
-# Wait for Vault to be unsealed
-echo "Waiting for Vault to be unsealed..."
-until curl -s --insecure "$VAULT_ADDR/v1/sys/seal-status" | jq -e '.sealed == false' > /dev/null; do
-  echo "Vault is still sealed... waiting 2 seconds"
-  sleep 2
-done
-echo "Vault is unsealed."
+wait_for_vault_ready()
+{
+  # Wait for Vault to be unsealed
 
-# Login to Vault using the client certificate
-echo "Waiting for Vault cert-auth to be ready..."
-until VAULT_TOKEN=$(vault login -method=cert \
-    -client-cert=/etc/elasticsearch/elasticsearch.crt \
-    -client-key=/etc/elasticsearch/elasticsearch.key \
-    -format=json | jq -r .auth.client_token); do
-  echo "Vault login failed (permission denied)... waiting..."
-  sleep 2
-done
+  echo "Waiting for Vault to be unsealed..."
+  until curl -s --insecure "$VAULT_ADDR/v1/sys/seal-status" | jq -e '.sealed == false' > /dev/null; do
+    echo "Vault is still sealed... waiting 2 seconds"
+    sleep 2
+  done
+  echo "Vault is unsealed."
+
+  echo "Waiting for Vault to be ready..."
+  until curl -s --insecure --fail "$VAULT_ADDR/v1/sys/health" | jq -e '.initialized == true and .sealed == false' > /dev/null; do
+    echo "Vault not ready... waiting 2 seconds"
+    sleep 2
+  done
+  echo "Vault is responding."
+}
+
+vault_cert_login()
+{
+  # Login to Vault using the client certificate
+
+  local cert_path="$1"
+  local key_path="$2"
+
+  echo "Waiting for Vault cert-auth to be ready..."
+  until vault login -method=cert \
+      -client-cert="$cert_path" \
+      -client-key="$key_path"; do
+    echo "Vault login failed (permission denied)... waiting..."
+    sleep 2
+  done
+
+  export VAULT_TOKEN=$(vault token lookup | jq -r .data.id)
+  echo "Vault login successful. Token: $VAULT_TOKEN"
+}
+
+# --- MAIN ---
+
+wait_for_vault_ready
+vault_cert_login "/etc/elasticsearch/elasticsearch.crt" "/etc/elasticsearch/elasticsearch.key"
 
 # Save Elasticsearch password non-interactively
 ELASTIC_PASSWORD=$(/usr/share/elasticsearch/bin/elasticsearch-reset-password --username elastic --auto --batch --silent)
@@ -49,16 +74,6 @@ KIBANA_TOKEN=$(/usr/share/elasticsearch/bin/elasticsearch-service-tokens create 
 echo "Storing Kibana service user token in Vault..."
 until vault kv put secret/kibana kibana_service_token="$KIBANA_TOKEN"; do
   echo "Failed to store Kibana service token in Vault... waiting..."
-  sleep 2
-done
-
-# Logstash save logstash_system user password
-LOGSTASH_PASSWORD=$(/usr/share/elasticsearch/bin/elasticsearch-reset-password --username logstash_system --auto --silent --batch)
-
-# Store the Logstash user password in Vault
-echo "Storing Logstash user password in Vault..."
-until vault kv put secret/logstash logstash_password="$LOGSTASH_PASSWORD"; do
-  echo "Failed to store Logstash password in Vault... waiting..."
   sleep 2
 done
 
--- services/monitoring/grafana/conf/start.sh
+++ services/monitoring/grafana/conf/start.sh
@@ -4,23 +4,48 @@ export VAULT_ADDR=https://vault:8200
 export VAULT_SKIP_VERIFY=true              # Self-signed certificates require this
 export HOME=/tmp
 
-# Wait for Vault to be unsealed
-echo "Waiting for Vault to be unsealed..."
-until curl -s --insecure "$VAULT_ADDR/v1/sys/seal-status" | jq -e '.sealed == false' > /dev/null; do
-  echo "Vault is still sealed... waiting 2 seconds"
-  sleep 2
-done
-echo "Vault is unsealed."
+wait_for_vault_ready()
+{
+  # Wait for Vault to be unsealed
 
-# Login to Vault using the client certificate
-echo "Waiting for Vault cert-auth to be ready..."
-until VAULT_TOKEN=$(vault login -method=cert \
-    -client-cert=/etc/grafana/certs/grafana.crt \
-    -client-key=/etc/grafana/certs/grafana.key \
-    -format=json | jq -r .auth.client_token); do
-  echo "Vault login failed (permission denied)... waiting..."
-  sleep 2
-done
+  echo "Waiting for Vault to be unsealed..."
+  until curl -s --insecure "$VAULT_ADDR/v1/sys/seal-status" | jq -e '.sealed == false' > /dev/null; do
+    echo "Vault is still sealed... waiting 2 seconds"
+    sleep 2
+  done
+  echo "Vault is unsealed."
+
+  echo "Waiting for Vault to be ready..."
+  until curl -s --insecure --fail "$VAULT_ADDR/v1/sys/health" | jq -e '.initialized == true and .sealed == false' > /dev/null; do
+    echo "Vault not ready... waiting 2 seconds"
+    sleep 2
+  done
+  echo "Vault is responding."
+}
+
+vault_cert_login()
+{
+  # Login to Vault using the client certificate
+
+  local cert_path="$1"
+  local key_path="$2"
+
+  echo "Waiting for Vault cert-auth to be ready..."
+  until vault login -method=cert \
+      -client-cert="$cert_path" \
+      -client-key="$key_path"; do
+    echo "Vault login failed (permission denied)... waiting..."
+    sleep 2
+  done
+
+  export VAULT_TOKEN=$(vault token lookup | jq -r .data.id)
+  echo "Vault login successful. Token: $VAULT_TOKEN"
+}
+
+# --- MAIN ---
+
+wait_for_vault_ready
+vault_cert_login "/etc/grafana/certs/grafana.crt" "/etc/grafana/certs/grafana.key"
 
 RAND=$(openssl rand -hex 16)
 
